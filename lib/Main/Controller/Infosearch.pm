3a884f64d8d00fbb1c2d76c51b8efd99647505ae
Anir Pandit <anir_pandit@hotmail.com>
2015-10-12 10:32:01 +0100

    121015v1

--- lib/Main/Controller/Infosearch.pm
+++ lib/Main/Controller/Infosearch.pm
@@ -40,23 +40,37 @@ sub infosearchw {
     
     #Info Search Form Queries#
     my $species_cond = "SELECT DISTINCT SpeciesInfo.speciesID FROM SpeciesInfo";
+    my $pep_cond = "SELECT neuropeptideID FROM NeuropeptideInfo";
+    my $func_cond = "SELECT funcID FROM FuncCategories";
     
+
     if($species_array[0]){
         $species_cond = join( ',', map { '?' } @species_array );
     }
     
-    my $pep_cond = "SELECT neuropeptideID FROM NeuropeptideInfo";
-    
+  
     if($pep_array[0]){
         $pep_cond = join( ',', map { '?' } @pep_array );
-        my $pep_string = join(',', @pep_array);
-        if(!$species_array[0]){
-            $species_cond = 'SELECT DISTINCT NeuroPepIsoInfo.speciesID FROM NeuroPepIsoInfo WHERE NeuroPepIsoInfo.neuropeptideID IN ('.$pep_string.')';
-        }
-    }
+    }    
+
+    my $query = '
+    SELECT DISTINCT SpeciesInfo.SpeciesName, OrderInfo.OrderName, SpeciesInfo.CommonName,
+    SpeciesInfo.Importance, SpeciesInfo.GenomeSequence, SpeciesInfo.GenomeDatabase, SpeciesInfo.DatabaseURL, SpeciesInfo.SpeciesSource
+    FROM SpeciesInfo, OrderInfo, NeuropeptideInfo, NeuroPepIsoInfo
+    WHERE SpeciesInfo.orderID = OrderInfo.orderID
+    AND NeuroPepIsoInfo.speciesID = SpeciesInfo.speciesID
+    AND NeuroPepIsoInfo.neuropeptideID = NeuropeptideInfo.neuropeptideID
+    AND ( SpeciesInfo.speciesID IN (' . $species_cond. ') AND NeuropeptideInfo.neuropeptideID IN (' . $pep_cond . '))
+    ORDER BY SpeciesInfo.speciesID';
     
-    my $func_cond = "SELECT funcID FROM FuncCategories";
-    
+    my $query2 = '
+    SELECT DISTINCT NeuroPepIsoInfo.isoID, NeuroPepIsoInfo.IsoformName, NeuroPepIsoInfo.IsoformAASeq, NeuroPepIsoInfo.Isoform_p_end, NeuroPepIsoInfo.Isoform_a_end, NeuropeptideInfo.NeuropeptideName, SpeciesInfo.SpeciesName, NeuroPepIsoInfo.GenBankAscNum, NeuroPepIsoInfo.GenBankAscNumURL, NeuropeptideInfo.neuropeptideID, NeuropeptideInfo.NeuropeptideDesc
+    FROM NeuroPepIsoInfo, NeuropeptideInfo, SpeciesInfo
+    WHERE NeuroPepIsoInfo.speciesID = SpeciesInfo.speciesID
+    AND NeuroPepIsoInfo.neuropeptideID = NeuropeptideInfo.neuropeptideID
+    AND ( SpeciesInfo.speciesID IN (' . $species_cond. ') AND NeuropeptideInfo.neuropeptideID IN (' . $pep_cond . '))
+    ORDER BY SpeciesInfo.speciesID';
+
     if($func_array[0]){
         $func_cond = join( ',', map { '?' } @func_array );
         my $func_string = join(',',@func_array);
@@ -67,22 +81,6 @@ sub infosearchw {
             $species_cond = 'SELECT DISTINCT FuncInfo.speciesID FROM FuncInfo WHERE FuncInfo.funcID IN ('.$func_string.')';
         }
     }
-    
-    my $query = '
-    SELECT DISTINCT SpeciesInfo.SpeciesName, OrderInfo.OrderName, SpeciesInfo.CommonName,
-    SpeciesInfo.Importance, SpeciesInfo.GenomeSequence, SpeciesInfo.GenomeDatabase, SpeciesInfo.DatabaseURL, SpeciesInfo.SpeciesSource
-    FROM SpeciesInfo, OrderInfo
-    WHERE SpeciesInfo.orderID = OrderInfo.orderID
-    AND (SpeciesInfo.speciesID IN (' . $species_cond. '))
-    ORDER BY SpeciesInfo.speciesID';
-    
-    my $query2 = '
-    SELECT DISTINCT NeuroPepIsoInfo.isoID, NeuroPepIsoInfo.IsoformName, NeuroPepIsoInfo.IsoformAASeq, NeuroPepIsoInfo.Isoform_p_end, NeuroPepIsoInfo.Isoform_a_end, NeuropeptideInfo.NeuropeptideName, SpeciesInfo.SpeciesName, NeuroPepIsoInfo.GenBankAscNum, NeuroPepIsoInfo.GenBankAscNumURL, NeuropeptideInfo.neuropeptideID, NeuropeptideInfo.NeuropeptideDesc
-    FROM NeuroPepIsoInfo, NeuropeptideInfo, SpeciesInfo
-    WHERE NeuroPepIsoInfo.speciesID = SpeciesInfo.speciesID
-    AND NeuroPepIsoInfo.neuropeptideID = NeuropeptideInfo.neuropeptideID
-    AND ( SpeciesInfo.speciesID IN (' . $species_cond. ') AND NeuropeptideInfo.neuropeptideID IN (' . $pep_cond . '))
-    ORDER BY SpeciesInfo.speciesID';
     
     my $query4 = '
     SELECT DISTINCT SpeciesInfo.SpeciesName, NeuropeptideInfo.NeuropeptideName, NeuropeptideInfo.NeuropeptideDesc,NeuroPepIsoInfo.IsoformName, FuncCategories.FuncCategoryName, FuncInfo.FuncDescription, FuncInfo.FuncURL, FuncInfo.idID
@@ -103,16 +101,16 @@ sub infosearchw {
     AND ( SpeciesInfo.speciesID IN (' . $species_cond . ') AND NeuropeptideInfo.neuropeptideID IN (' . $pep_cond . ') AND FuncCategories.funcID IN (' . $func_cond . '))';
     
     my $sth = $dbh->prepare($query);
-    $sth->execute(@species_array);
+    $sth->execute(@species_array,@pep_array);
     
     my $sth2 = $dbh->prepare($query2);
     $sth2->execute(@species_array,@pep_array);
     
     my $sth4 = $dbh->prepare($query4);
-    $sth4->execute(@species_array,@pep_array,@func_array,);
+    $sth4->execute(@species_array,@pep_array,@func_array);
     
     my $sth5 = $dbh->prepare($query5);
-    $sth5->execute(@species_array,@pep_array,@func_array,);
+    $sth5->execute(@species_array,@pep_array,@func_array);
     
     $self->stash(
     species => $species_array[0],
