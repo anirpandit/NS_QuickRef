8bcca597b9cdc7d31fba06cfd5ee3b89bfe6170c
Anir Pandit <anir_pandit@hotmail.com>
2015-12-02 15:25:02 +0000

    second

--- Makefile.pl
+++ Makefile.pl
@@ -6,7 +6,7 @@ use ExtUtils::MakeMaker;
 WriteMakefile(
   NAME         => 'ns_quickrefnew.pl',
   VERSION      => '1.0',
-  AUTHOR       => 'Magnus Holm <judofyr@gmail.com>',
+  AUTHOR       => 'Anir Pandit <anir_pandit@yahoo.com>',
   EXE_FILES    => ['ns_quickrefnew.pl'],
   PREREQ_PM    => {'Mojolicious' => '2.0'},
   test         => {TESTS => 't/*.t'}
